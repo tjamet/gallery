abfdaa544ade8d7fc3c72b51b56ebcc2bc2ee281
Thibault Jamet <tjamet@users.noreply.github.com>
2017-07-24 23:12:32 +0200

    Add a filter to select files only
    Signed-off-by: Thibault Jamet <tjamet@users.noreply.github.com>

---
+++ filters/file.go
@@ -0,0 +1,15 @@
+package filters
+
+type InfoProvider interface {
+	IsDir() bool
+}
+
+// IsFile returns true if the file provided as parameter is
+// a file, false otherwise
+func IsFile(info InfoProvider) bool {
+	return !info.IsDir()
+}
+
+
+
+
